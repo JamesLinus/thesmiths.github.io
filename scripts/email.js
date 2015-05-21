cc0d61fd3a844b1b38823a0ff57840d48967880f
Matthias Benkort <matthias.benkort@gmail.com>
2015-05-21 10:15:34 +0200

    Adapt old script css

--- scripts/email.js
+++ scripts/email.js
@@ -2,15 +2,8 @@ $(document).ready(function () {
     Parse.initialize("J9U1LbAaB7LoxtCnh6jpyJYxRlRRdrMImPnNdzS6", "F8zgXvM68Xcmco6rHvFzAeZ9OyQFhECW6iw4T07n");
     $("#contact").on('submit', function (e) {
         e.preventDefault();
-        $("#submit-down").attr('disabled', true).fadeOut("fast");
+        $("#send").attr('disabled', true);
         $("#contact-message").html("");
-        
-        $("#contact-loader").show();
-        (function animateLoader () {
-            $("#contact-loader")
-                .animate({left: '5%', opacity: 1}, 0)
-                .animate({left: "95%", opacity: 0}, 1500, animateLoader);
-        })();
 
         var data = {
             email: $('input[name="email"]').val(),
@@ -19,22 +12,19 @@ $(document).ready(function () {
         };
 
         setTimeout(function (msg) {
-                $("#contact-loader").hide().stop();
-                $("#contact-message").html(msg).addClass("success").fadeIn('fast'); 
-                $("#submit-down").attr('disabled', false).fadeIn("fast");
-        }, 800, "sent");
+                $("#contact-message").html(msg).addClass("error"); 
+                $("#send").attr('disabled', false);
+        }, 800, "An error occured");
 
         return;
         Parse.Cloud.run("sendEmail", data, {
             success: function (msg) {
-                $("#contact-loader").hide().stop();
                 $("#contact-message").html(msg).addClass("success").fadeIn('fast'); 
-                $("#submit-down").attr('disabled', false).fadeIn("fast");
+                $("#send").attr('disabled', false);
             }, 
             error: function (msg) {
-                $("#contact-loader").hide().finish();
                 $("#contact-message").html(msg).addClass("error").fadeIn('fast'); 
-                $("#submit-down").attr('disabled', false).fadeIn("fast");
+                $("#send").attr('disabled', false);
             }
         });
     });
